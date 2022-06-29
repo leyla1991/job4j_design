d9506cd54982ec7d36265715586e90c5c6922280
leyla1991 <atagleyla@mail.ru>
2022-06-29 05:17:45 +0300

    5. Очередь на двух стеках [#160]

---
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -0,0 +1,27 @@
+package ru.job4j.collection;
+
+import java.util.NoSuchElementException;
+
+public class SimpleQueue<T> {
+
+    private final SimpleStack<T> in = new SimpleStack<>();
+    private final SimpleStack<T> out = new SimpleStack<>();
+
+    public T poll() {
+        if (in == null) {
+            throw new NoSuchElementException();
+        }
+        while (!in.isEmpty()) {
+            out.push(in.pop());
+        }
+        T result = out.pop();
+        while (!out.isEmpty()) {
+            in.push(out.pop());
+        }
+        return result;
+    }
+
+    public void push(T value) {
+        in.push(value);
+    }
+}
--- src/main/java/ru/job4j/collection/SimpleStack.java
+++ src/main/java/ru/job4j/collection/SimpleStack.java
@@ -3,12 +3,20 @@ package ru.job4j.collection;
 public class SimpleStack<T> {
 
     private ForwardLinked<T> linked = new ForwardLinked<T>();
+    private int size = -1;
 
     public T pop() {
+        size--;
         return  linked.deleteFirst();
+
     }
 
     public void push(T value) {
         linked.addFirst(value);
+        size++;
+    }
+
+    public boolean isEmpty() {
+        return size == -1;
     }
 }
