48087cfd69ec9b43e0970ca29d071a3958b57156
leyla1991 <atagleyla@mail.ru>
2022-08-11 16:12:49 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/Matrix.java
@@ -0,0 +1,20 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class Matrix {
+
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("multiple.txt")) {
+            for (int i = 1; i < 10; i++) {
+                for (int x = 1; x < 10; x++) {
+                     out.write(((i * x) + " ").getBytes(StandardCharsets.UTF_8));
+                }
+                out.write(System.lineSeparator().getBytes(StandardCharsets.UTF_8));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
