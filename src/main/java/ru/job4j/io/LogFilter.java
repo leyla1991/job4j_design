ab938246cd3a2bcb4c1d2522ab23dd439970f54b
leyla1991 <atagleyla@mail.ru>
2022-08-15 18:55:38 +0300

    0.3. BufferedReader. [#252489]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -13,8 +13,8 @@ public class LogFilter {
        try (BufferedReader in = new BufferedReader(new FileReader("log.txt"))) {
            for (String line = in.readLine(); line != null; line = in.readLine()) {
                String[] lines = line.split(" ");
-                   if (lines[lines.length - 2].equals("404")) {
-                       list.add(line);
+               if ("404".equals(lines[lines.length - 2])) {
+                   list.add(line);
                }
            }
        } catch (IOException e) {
@@ -26,7 +26,8 @@ public class LogFilter {
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
         List<String> log = logFilter.filter("log.txt");
-        System.out.println(log);
-
+        for (String l : log) {
+            System.out.println(l);
+        }
     }
 }
