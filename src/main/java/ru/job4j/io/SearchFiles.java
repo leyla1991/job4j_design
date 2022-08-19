e34228cb5a9c56ccf20538910cfa5a83513e85ef
leyla1991 <atagleyla@mail.ru>
2022-08-19 11:42:17 +0300

    4.1. Сканирование файловой системы. [#106929]

---
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -0,0 +1,32 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+public class SearchFiles extends SimpleFileVisitor<Path> {
+
+    List<Path> root = new ArrayList<>();
+    Predicate<Path> condition;
+
+    public SearchFiles(Predicate<Path> condition) {
+        this.condition = condition;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (condition.test(file)) {
+            root.add(file.toAbsolutePath());
+        }
+        return CONTINUE;
+    }
+
+    public List<Path> getPaths() {
+        return root;
+    }
+}
