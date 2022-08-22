9218c1f02855a4815d6acc544c53a822edf98681
leyla1991 <atagleyla@mail.ru>
2022-08-23 00:46:45 +0300

    5.1. Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,60 @@
+package ru.job4j.io;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException("Wrong key");
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        validates(args);
+        for (String s : args) {
+            String[] kv = s.replaceFirst("-", "").split("=", 2);
+            if (kv.length == 2) {
+                values.put(kv[0], kv[1]);
+            }
+        }
+
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void validates(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Wrong values");
+        }
+        for (String arg : args) {
+             if (!arg.contains("=")) {
+                 throw new IllegalArgumentException("Not found =");
+             }
+             if (!arg.startsWith("-")) {
+                 throw new IllegalArgumentException("Not found -");
+             }
+             String[] a = arg.replaceFirst("-", "").split("=", 2);
+             if (a[0].isEmpty()) {
+                 throw new IllegalArgumentException("Not found key");
+             }
+        }
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
