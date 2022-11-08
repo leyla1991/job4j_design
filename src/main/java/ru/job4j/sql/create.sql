77e4d06649b79ea74b99ca630a7fd944b202770c
leyla1991 <atagleyla@mail.ru>
2022-11-08 16:51:40 +0300

    1. Схема прав пользователи и роли. [#1093]

---
+++ src/main/java/ru/job4j/sql/create.sql
@@ -0,0 +1,56 @@
+create table roles(
+	id serial primary key,
+	name varchar(255)
+);
+
+create table category(
+	id serial primary key,
+	number int,
+	name varchar(255)
+);
+
+create table stateItem(
+	id serial primary key,
+	name varchar(255)
+);
+
+create table item(
+	id serial primary key,
+	number int,
+	name varchar(255),
+	category_id int references category(id),
+	stateItem_id int references stateItem(id)
+);
+
+create table users(
+	id serial primary key,
+	name varchar(255),
+	roles_id int references roles(id),
+	item_id int references item(id)
+);
+
+
+create table rules(
+	id serial primary key,
+	name text
+);
+
+create table commentsItem(
+	id serial primary key,
+	name text,
+	item_id int references item(id)
+);
+
+
+
+create table attachs(
+	id serial primary key,
+	name varchar(255),
+	item_id int references item(id)
+);
+
+create table roles_rules(
+	id serial primary key,
+	roles_id int references roles(id),
+	rules_id int references rules(id)
+);
---
+++ src/main/java/ru/job4j/sql/insert.sql
@@ -0,0 +1,37 @@
+insert into roles(name) values ('Romeo');
+insert into roles(name) values ('Julieta');
+insert into roles(name) values ('Rozalin');
+
+insert into category(number, name) values (1, 'first');
+insert into category(number, name) values (2, 'second');
+insert into category(number, name) values (3, 'third');
+
+insert into stateItem(name) values ('close');
+insert into stateItem(name) values ('open');
+
+insert into item(number, name, category_id, stateItem_id) values ((1), 'find Man', 1, 2);
+insert into item(number, name, category_id, stateItem_id) values ((1), 'find Women', 2, 1);
+insert into item(number, name, category_id, stateItem_id) values ((1), 'find Women', 3, 2);
+
+insert into users(name, roles_id, item_id) values ('Ivan', 1, 1);
+insert into users(name, roles_id, item_id) values ('Anna', 2, 3);
+insert into users(name, roles_id, item_id) values ('Luiza', 2, 2);
+insert into users(name, roles_id, item_id) values ('Anton', 1, 1);
+insert into users(name, roles_id, item_id) values ('Anastasia', 2, 2);
+
+insert into rules(name) values ('лунный свет');
+insert into rules(name) values ('Romeo!');
+insert into rules(name) values ('Noo');
+
+insert into commentsItem(name, item_id) values ('bravo', 1);
+insert into commentsItem(name, item_id) values ('no', 2);
+insert into commentsItem(name, item_id) values ('brave', 3);
+
+insert into attachs(name, item_id) values ('rules', 1);
+insert into attachs(name, item_id) values ('rules', 2);
+insert into attachs(name, item_id) values ('rules', 3);
+
+insert into roles_rules(roles_id, rules_id) values (1, 1);
+insert into roles_rules(roles_id, rules_id) values (2, 2);
+insert into roles_rules(roles_id, rules_id) values (3, 3);
+insert into roles_rules(roles_id, rules_id) values (2, 1);
