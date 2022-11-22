e193961c90f4c89dd28258d9429b7deea08a155e
leyla1991 <atagleyla@mail.ru>
2022-11-22 21:34:53 +0300

    1. Inner join [#373617]

--- src/main/java/ru/job4j/sql/inner_join.sql
+++ src/main/java/ru/job4j/sql/inner_join.sql
@@ -2,32 +2,33 @@ create table book(
 	id serial primary key,
 	name varchar(255),
 	page int
+	author_id int references author(id)
 );
 
 create table author(
 	id serial primary key,
 	name varchar(255),
-	book_id int references book(id)
+
 );
 
-insert into book(name, page) values ('Маленький принц', 255);
-insert into book(name, page) values ('It', 405);
+insert into book(name, page, author_id) values ('Маленький принц', 255, 1);
+insert into book(name, page, author_id) values ('It', 405, 2);
 insert into book(name, page) values ('Shorts', 55);
-insert into book(name, page) values ('Gamlet', 605);
+insert into book(name, page, author_id) values ('Gamlet', 605, 5);
 insert into book(name, page) values ('Key', 900);
 
-insert into author(name, book_id) values ('Экзепьюри', 1);
-insert into author(name, book_id) values ('Кинг', 2);
+insert into author(name) values ('Экзепьюри');
+insert into author(name) values ('Кинг');
 insert into author(name) values ('Василий');
 insert into author(name) values ('Порто');
-insert into author(name, book_id) values ('Шекспир', 4);
+insert into author(name) values ('Шекспир');
 insert into author(name) values ('Шекспириус');
 
 select a.name, b.name, b.page
-from author as a join book as b on a.book_id = b.id;
+from author as a join book as b on b.author_id = a.id;
 
 select a.name as Фамилия, b.name as Название, b.page as Страницы
-from author as a join book as b on a.book_id = b.id;
+from author as a join book as b on b.author_id = a.id;
 
 select a.name as "Фамилия автора", b.name  Название, b.page Страницы
-from author a join book b on a.book_id = b.id;
+from author a join book b on a.author_id = a.id;
