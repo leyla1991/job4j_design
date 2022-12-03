893adcb88ba2e81c0594de3af9d2519c3f992f6f
leyla1991 <atagleyla@mail.ru>
2022-12-03 20:36:44 +0300

    1. Inner join [#373617]

--- src/main/java/ru/job4j/sql/inner_join.sql
+++ src/main/java/ru/job4j/sql/inner_join.sql
@@ -10,18 +10,18 @@ create table book(
 	author_id int references author(id)
 );
 
-insert into book(name, page, author_id) values ('Маленький принц', 255, 1);
-insert into book(name, page, author_id) values ('It', 405, 2);
-insert into book(name, page) values ('Shorts', 55);
-insert into book(name, page, author_id) values ('Gamlet', 605, 5);
-insert into book(name, page) values ('Key', 900);
-
 insert into author(name) values ('Экзепьюри');
 insert into author(name) values ('Кинг');
 insert into author(name) values ('Василий');
 insert into author(name) values ('Порто');
 insert into author(name) values ('Шекспир');
 insert into author(name) values ('Шекспириус');
+
+insert into book(name, page, author_id) values ('Маленький принц', 255, 1);
+insert into book(name, page, author_id) values ('It', 405, 2);
+insert into book(name, page) values ('Shorts', 55);
+insert into book(name, page, author_id) values ('Gamlet', 605, 5);
+insert into book(name, page) values ('Key', 900);
 
 select a.name, b.name, b.page
 from author as a join book as b on b.author_id = a.id;
