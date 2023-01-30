50f194661438c58ae8452dd5656a5d23346dc30b
leyla1991 <atagleyla@mail.ru>
2023-01-30 07:37:53 +0300

    Хранилище машин [#1733 #368055]

--- src/main/java/ru/job4j/sql/cars_select.sql
+++ src/main/java/ru/job4j/sql/cars_select.sql
@@ -2,20 +2,20 @@ SELECT c.id, c.name AS car_name, cb.name AS body_name, ce.name AS engine_name, c
 FROM
 	cars c
 	LEFT OUTER JOIN car_bodies cb ON cb.id = c.body_id
-	FULL OUTER JOIN car_engines ce ON ce.id = c.engine_id
-	FULL OUTER JOIN car_transmissions ct ON ct.id = c.transmission_id;
+	LEFT OUTER JOIN car_engines ce ON ce.id = c.engine_id
+	LEFT OUTER JOIN car_transmissions ct ON ct.id = c.transmission_id;
 
 SELECT cb.name
 FROM car_bodies cb LEFT JOIN cars c
 	ON cb.id = c.body_id
 WHERE c.body_id IS null;
 
+SELECT ct.name
+FROM car_transmissions ct LEFT JOIN cars c
+	ON ct.id = c.transmission_id
+WHERE transmission_id IS null;
+
 SELECT ce.name
 FROM car_engines ce LEFT JOIN cars c
 	ON ce.id = c.engine_id
-WHERE c.body_id IS null;
-
-SELECT ct.name
-FROM car_transmissions ct LEFT JOIN cars c
-	ON ct.id = c.body_id
-WHERE c.body_id IS null;
+WHERE engine_id IS null;
