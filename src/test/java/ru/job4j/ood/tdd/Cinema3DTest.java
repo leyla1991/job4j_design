f44ad4fc5633ed153dfe26e82bf7b0662449744b
leyla1991 <atagleyla@mail.ru>
2023-07-26 14:10:11 +0300

    3. Что такое TDD? [#4918  [#4918]]

---
+++ src/test/java/ru/job4j/ood/tdd/Cinema3DTest.java
@@ -0,0 +1,67 @@
+package ru.job4j.ood.tdd;
+
+import org.checkerframework.checker.units.qual.C;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.TimeZone;
+
+class Cinema3DTest {
+
+    @Test
+    public void whenBuyThenGetTicket() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket).isEqualTo(new Ticket3D());
+    }
+
+    @Test
+    public void whenAddSessionThenItExistsBetweenAllSessions() {
+        Cinema cinema = new Cinema3D();
+        Session session = new Session3D();
+        cinema.add(session);
+        List<Session> sessions = cinema.find(session1 -> true);
+        assertThat(sessions).contains(session);
+
+    }
+
+    @Test
+    public void whenBuyOnInvalidRowThenGetException() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        assertThatThrownBy(() -> cinema.buy(account, -1, 1, date))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void whenBuyInvalidColumnThenGetException() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        assertThatThrownBy(() -> cinema.buy(account, 1, 0, date))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void whenAccountIsNull() {
+        Calendar calendar = Calendar.getInstance();
+        Cinema cinema = new Cinema3D();
+        assertThatThrownBy(() -> cinema.buy(null, 1, 1, calendar))
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    public void whenMistakeDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        assertThatThrownBy(() -> cinema.buy(account, 1, 1, null))
+                .isInstanceOf(NullPointerException.class);
+    }
+
+}
